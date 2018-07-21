09fa6de1b0dd135f0778ab8078de40d657cfe666
ysk2014 <1181102772@qq.com>
2018-07-21 23:34:01 +0800

    change

--- generator.js
+++ generator.js
@@ -1,4 +1,9 @@
+const path = require("path");
+
 module.exports = (api, meta, options) => {
     api.render("./template", meta.path);
     
+    if (meta.route) {
+        api.injectVueRoutes(meta.routePath, api.renderToString(path.join(__dirname, "./route.js"), meta));
+    }
 }
